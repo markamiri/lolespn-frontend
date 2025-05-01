b569bfdfb84386f6c158309a4c18606e7dd69621
markamiri <103591552+markamiri@users.noreply.github.com>
2025-05-01 06:32:15 -0400

    fixed overflow x mobile

--- src/app/players/[id]/page.tsx
+++ src/app/players/[id]/page.tsx
@@ -2,7 +2,7 @@ import Navbar from "@/components/navbar";
 import HeaderGameLog from "@/components/headergamelog";
 export default function PlayerPage() {
   return (
-    <div className="min-h-screen bg-[rgb(237,238,238,255)]">
+    <div className="min-h-screen bg-[rgb(237,238,238,255)] overflow-x-hidden w-full">
       <Navbar />
       <HeaderGameLog />
     </div>
